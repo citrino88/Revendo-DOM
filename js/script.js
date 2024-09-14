38cd9cfcd6cfa70550a6beaec1507f17d9627cb5
Almeida Dayane <almeida.citrino@gmail.com>
2024-09-14 08:46:16 -0300

    revisao

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* *******  REVISÃO DOS DIAS ANTERIORES  *********
+
+const titulo = document.querySelector('#tituloPrincipal');
+
+titulo.textContent = 'Teste Dom';
+titulo.style.backgroundColor = 'red';
+
+titulo.addEventListener('dblclick', ()=> alert('teste') ); //dbl = double click
+// mouseleave, quando sai 
+
+
+ */
