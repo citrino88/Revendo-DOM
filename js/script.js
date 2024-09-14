1d06ad1031f1a85a823ed16b8094fc6202a54fdb
Almeida Dayane <almeida.citrino@gmail.com>
2024-09-14 11:24:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ titulo.addEventListener('dblclick', ()=> alert('teste') ); //dbl = double click
 do HTML: Nós de atributos
     >> pegarClasse, usando evento com botão
    ---------------- */
-
+/* 
 const titulo = document.querySelector('#titulo');
 const resposta = document.querySelector('#respostaAtributo');
 
@@ -24,36 +24,78 @@ function pegarClasse() {
 }
 
 const btnClasse = document.querySelector('#pegarClasse'); // Encontrando o botão no DOM
-btnClasse.addEventListener('click', pegarClasse); // Adicionando evento click
+btnClasse.addEventListener('click', pegarClasse); // Adicionando evento click */
 
 
 /* >> verificarClasse, usando evento com botão
    ---------------- */
 
-function verificar() {
+/* function verificar() {
     resposta.textContent = titulo.hasAttribute('class');
 }  
 
 const btnVerificar = document.querySelector('#verificarClasse');
-btnVerificar.addEventListener('click', verificar);
+btnVerificar.addEventListener('click', verificar); */
 
 /* >> trocarClasse, usando evento com botão
    ---------------- */
 
-function trocar() {
+/* function trocar() {
     titulo.setAttribute('class', 'tituloFundoRosa');
 } 
 
 const btnTrocar = document.querySelector('#trocarClasse');
-btnTrocar.addEventListener('click', trocar);
+btnTrocar.addEventListener('click', trocar); */
    
 /* >> variarClasse, usando evento com botão
    ---------------- */   
 
-function variar()   {
+/* function variar()   {
     
     titulo.classList.toggle('tituloVermelho');
 }
 
 const btnVariar = document.querySelector('#variarClasse');
-btnVariar.addEventListener('click', variar);
+btnVariar.addEventListener('click', variar); */
+
+/* >> Acender lâmpada, trocar cor de fundo, trocar texto para Apagar
+   ---------------- */  
+
+/* const lampada = document.querySelector('#lampada');
+const varia = document.querySelector('#variar');
+const body = document.querySelector('body');
+
+varia.addEventListener("click", function acender(){
+if (varia.textContent == "Acender"){
+    lampada.setAttribute("src", "midia/img/acesa.png");
+    body.classList.toggle('acesa');
+    varia.textContent = "Apagar";
+}else{
+    varia.textContent = "Acender";
+    lampada.setAttribute("src", "midia/img/apagada.png");
+    body.classList.toggle('acesa');
+}
+}); */
+
+var estado = 'apagada';
+function alterarLampada(){
+    const lampada = document.querySelector('#lampada');
+    const body = document.querySelector('body');
+
+    if(estado == 'apagada'){
+        btnVariar.textContent = 'Acender';
+        estado = 'aceso';
+    }else{
+        btnVariar.textContent = 'Apagar';
+        estado = 'apagada';
+    }
+    
+}
+
+/* >> Desafio Jokempô
+   ---------------- */
+
+const tesoura = document.querySelector('#tesoura');
+const pedra = document.querySelector('#pedra');
+const papel = document.querySelector('#papel');
+const respostaJokempo = document.querySelector('#respostaJokempo');
